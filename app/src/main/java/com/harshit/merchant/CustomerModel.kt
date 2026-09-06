f09bde08b696950eddf4a3ecf33cb18272a7ffd8
Harsh89007 <151430440+Harsh89007@users.noreply.github.com>
2026-09-07 01:21:58 +0530

    Update CustomerModel.kt

--- app/src/main/java/com/harshit/merchant/CustomerModel.kt
+++ app/src/main/java/com/harshit/merchant/CustomerModel.kt
@@ -1,14 +1,21 @@
 package com.harshit.merchant
 
 data class CustomerModel(
+    var loanId: String = "",
     var imei: String = "",
     var name: String = "",
     var mobile: String = "",
     var email: String = "",
     var photoUrl: String = "",
+    var remarks: String = "N/A",
+    var syncDate: String = "",
+    var status: String = "Unlocked",
+    
+    // कंट्रोल्स
     var isDeviceLocked: Boolean = false,
-    var isAppLocked: Boolean = false,
-    var isCallLocked: Boolean = false,
-    var isSimLocked: Boolean = false,
+    var isYoutubeLocked: Boolean = false,
+    var isWhatsappLocked: Boolean = false,
+    var isInstagramLocked: Boolean = false,
+    var isPhonePeLocked: Boolean = false,
     var uninstallRequested: Boolean = false
 )
